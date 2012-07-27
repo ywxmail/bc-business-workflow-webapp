8a5ccedfdf647153d9012e81b66e302d2a22ffca
fatfish <fatfish2010@sina.cn>
2012-07-27 11:53:57 +0800

    汇总推出车辆流程表单修改

--- confirmretiredcars/GatherCars.js
+++ confirmretiredcars/GatherCars.js
@@ -16,154 +16,7 @@ bswf.confirmretiredcars.GatherCarsForm = {
 				status: '0',
 				data:{multiple: true,loadLevel:'1'},
 				onOk : function(cars) {
-					function selectUnit(cell){
-						var $cell=$(cell);
-						$cell.click(function(){
-							bc.identity.selectUnit({
-								onOk : function(unit) {
-									$cell.find(":input[name='unitCompanyId']").val(unit.id);
-									$cell.find(":input[name='unitCompany']").val(unit.name);
-								}
-							});
-						})
-					}
-					
-					//先验证是否添加相同的车辆信息
-					var addflag=true;
-					if($(tableEl).find("tr").size()>1){
-						var $carTRs = $form.find("#cars tr:gt(0)");
-						$carTRs.each(function(){
-							$tr = $(this);
-							var $inputs = $tr.find(":input");
-							var trid=$inputs[0].value;
-							for(var i=0;i <cars.length;i++){
-								if(trid == cars[i].id){
-									addflag=false;
-									return;
-								}
-							}
-						});
-					}
-					
-					if(!addflag){
-						bc.msg.alert("选择的车辆信息与已添加的车辆信息相同！");
-						return;
-					}
-					
-					for(var i=0;i <cars.length;i++){
-						if($(tableEl).find("tr").size()==1 && $form.find(":input[name='verifyUnitId']").val()==''){
-							$form.find(":input[name='verifyUnitId']").val(cars[i].unitCompanyId);
-							$form.find(":input[name='verifyUnitName']").val(cars[i].unitCompany);
-						}
-						
-						//插入行
-						var newRow=tableEl.insertRow(tableEl.rows.length);
-						newRow.setAttribute("class","ui-widget-content row");
-						//插入列
-						var cell=newRow.insertCell(0);
-						cell.style.padding="0";
-						cell.style.textAlign="left";
-						cell.setAttribute("class","id first");
-						cell.innerHTML='<span class="ui-icon"></span><input class="ignore" type="hidden" name="id" value="'+cars[i].id+'"/>';//ID列
-						
-						//插入公司
-						cell=newRow.insertCell(1);
-						cell.style.padding="0";
-						cell.style.textAlign="left";
-						cell.setAttribute("class","middle");
-						cell.innerHTML='<input name="unitCompany" class="ignore"  style="border:none;background:none;width:4.9em;padding:0 0 0 2px"'
-							+'value="'
-							+cars[i].unitCompany+'"'
-							+'type="text"  data-validate="required">'
-							+'<input type="hidden" name="unitCompanyId" class="ignore" value="'+cars[i].unitCompanyId+'"/>';
-						selectUnit(cell);
-						
-						//插入车号
-						cell=newRow.insertCell(2);
-						cell.style.padding="0";
-						cell.style.textAlign="left";
-						cell.setAttribute("class","middle");
-						cell.innerHTML='<input name="plateNo" class="ignore" style="border:none;background:none;width:4.4em;padding:0 0 0 2px"'
-							+'value="'+cars[i].plateNo+'"'
-							+'type="text" readonly="readonly"  data-validate="required">';
-						
-						//插入营运性质
-						cell=newRow.insertCell(3);
-						cell.style.padding="0";
-						cell.style.textAlign="left";
-						cell.setAttribute("class","middle");
-						cell.innerHTML='<input name="businessType" class="ignore" style="border:none;background:none;width:4.9em;padding:0 0 0 2px;"'
-							+'value="'+cars[i].bsType+'"'
-							+'type="text"  data-validate="required">';
-						
-						//插入登记日期
-						cell=newRow.insertCell(4);
-						cell.style.padding="0";
-						cell.style.textAlign="left";
-						cell.setAttribute("class","middle");
-						cell.innerHTML='<input type="text"  name="registerDate" data-validate=\'{"type":"date","required":true}\' value="'+cars[i].registerDate+'"'
-							+'  style="border:none;background:none;width:6.5em;padding:0 0 0 2px;" class="ignore bc-date ui-widget-content" >';
-						//绑定日期选择
-						bc.form.initCalendarSelect($(cell));
-						
-						//插入合同期限
-						var ccEndDate='';
-						if(!(cars[i].ccEndDate==null))
-							ccEndDate=cars[i].ccEndDate.substring(0,10);
-						cell=newRow.insertCell(5);
-						cell.style.padding="0";
-						cell.style.textAlign="left";
-						cell.setAttribute("class","middle");
-						cell.innerHTML='<input type="text" name="ccEndDate" data-validate=\'{"type":"date","required":true}\' value="'+ccEndDate+'"'
-							+'  style="border:none;background:none;width:6.5em;padding:0 0 0 2px;" class="ignore bc-date ui-widget-content" >';
-						//绑定日期选择
-						bc.form.initCalendarSelect($(cell));
-						
-						//插入商业结束日期
-						var commerialEndDate='';
-						if(!(cars[i].commerialEndDate==null))
-							commerialEndDate=cars[i].commerialEndDate.substring(0,10);
-						cell=newRow.insertCell(6);
-						cell.style.padding="0";
-						cell.style.textAlign="left";
-						cell.setAttribute("class","middle");
-						cell.innerHTML='<input type="text" name="commerialEndDate" data-validate=\'{"type":"date","required":true}\' value="'+commerialEndDate+'"'
-							+'  style="border:none;background:none;width:6.5em;padding:0 0 0 2px;" class="ignore bc-date ui-widget-content"  >';
-						//绑定日期选择
-						bc.form.initCalendarSelect($(cell));
-						
-						//插入强险结束日期
-						var greenslipEndDate='';
-						if(!(cars[i].greenslipEndDate==null))
-							greenslipEndDate=cars[i].greenslipEndDate.substring(0,10);
-						cell=newRow.insertCell(7);
-						cell.style.padding="0";
-						cell.style.textAlign="left";
-						cell.setAttribute("class","middle");
-						cell.innerHTML='<input type="text" name="greenslipEndDate" data-validate=\'{"type":"date","required":true}\' value="'+greenslipEndDate+'"'
-							+'  style="border:none;background:none;width:6.5em;padding:0 0 0 2px;" class="ignore bc-date ui-widget-content" >';
-						//绑定日期选择
-						bc.form.initCalendarSelect($(cell));
-												
-						//插入预计交车日期
-						var predictReturnDate='';
-						if(!(cars[i].predictReturnDate==null))
-							predictReturnDate=cars[i].predictReturnDate.substring(0,10);
-						cell=newRow.insertCell(8);
-						cell.style.padding="0";
-						cell.style.textAlign="left";
-						cell.setAttribute("class","middle");
-						cell.innerHTML='<input type="text" name="predictReturnDate" data-validate=\'{"type":"date","required":true}\' value="'+predictReturnDate+'"'
-							+'  style="border:none;background:none;width:6.5em;padding:0 0 0 2px;" class="ignore bc-date ui-widget-content"  >';
-						//绑定日期选择
-						bc.form.initCalendarSelect($(cell));
-						
-						//插入空白列
-						cell=newRow.insertCell(9);
-						cell.style.minWidth="0.01em";
-						cell.style.borderLeftWidth="0";
-						cell.setAttribute("class","last");
-					}		
+					addCars(cars);
 				}
 			});
 		});
@@ -234,10 +87,168 @@ bswf.confirmretiredcars.GatherCarsForm = {
 				onOk : function(unit) {
 					$form.find(":input[name='verifyUnitId']").val(unit.id);
 					$form.find(":input[name='verifyUnitName']").val(unit.name);
+					bc.ajax({
+						url :bc.root+"/bc-business/car/findRetiredCarsOfNextMonth?unitId="+unit.id,
+						dataType : "json",
+						success : function(cars){
+							addCars(cars);
+						}
+					});
 				}
 			});
 		});
+			
+		
+		function  addCars(cars){
+			//先验证是否添加相同的车辆信息
+			var addflag=true;
+			if($(tableEl).find("tr").size()>1){
+				var $carTRs = $form.find("#cars tr:gt(0)");
+				$carTRs.each(function(){
+					$tr = $(this);
+					var $inputs = $tr.find(":input");
+					var trid=$inputs[0].value;
+					for(var i=0;i <cars.length;i++){
+						if(trid == cars[i].id){
+							addflag=false;
+							return;
+						}
+					}
+				});
+			}
+			
+			if(!addflag){
+				bc.msg.alert("选择的车辆信息与已添加的车辆信息相同！");
+				return;
+			}
+			
+			for(var i=0;i <cars.length;i++){
+				if($(tableEl).find("tr").size()==1 && $form.find(":input[name='verifyUnitId']").val()==''){
+					$form.find(":input[name='verifyUnitId']").val(cars[i].unitCompanyId);
+					$form.find(":input[name='verifyUnitName']").val(cars[i].unitCompany);
+				}
 				
+				//插入行
+				var newRow=tableEl.insertRow(tableEl.rows.length);
+				newRow.setAttribute("class","ui-widget-content row");
+				//插入列
+				var cell=newRow.insertCell(0);
+				cell.style.padding="0";
+				cell.style.textAlign="left";
+				cell.setAttribute("class","id first");
+				cell.innerHTML='<span class="ui-icon"></span><input class="ignore" type="hidden" name="id" value="'+cars[i].id+'"/>';//ID列
+				
+				//插入公司
+				cell=newRow.insertCell(1);
+				cell.style.padding="0";
+				cell.style.textAlign="left";
+				cell.setAttribute("class","middle");
+				cell.innerHTML='<input name="unitCompany" class="ignore"  style="border:none;background:none;width:4.9em;padding:0 0 0 2px"'
+					+'value="'
+					+cars[i].unitCompany+'"'
+					+'type="text"  data-validate="required">'
+					+'<input type="hidden" name="unitCompanyId" class="ignore" value="'+cars[i].unitCompanyId+'"/>';
+				selectUnit(cell);
+				
+				//插入车号
+				cell=newRow.insertCell(2);
+				cell.style.padding="0";
+				cell.style.textAlign="left";
+				cell.setAttribute("class","middle");
+				cell.innerHTML='<input name="plateNo" class="ignore" style="border:none;background:none;width:4.4em;padding:0 0 0 2px"'
+					+'value="'+cars[i].plateNo+'"'
+					+'type="text" readonly="readonly"  data-validate="required">';
+				
+				//插入营运性质
+				cell=newRow.insertCell(3);
+				cell.style.padding="0";
+				cell.style.textAlign="left";
+				cell.setAttribute("class","middle");
+				cell.innerHTML='<input name="businessType" class="ignore" style="border:none;background:none;width:4.9em;padding:0 0 0 2px;"'
+					+'value="'+cars[i].bsType+'"'
+					+'type="text"  data-validate="required">';
+				
+				//插入登记日期
+				cell=newRow.insertCell(4);
+				cell.style.padding="0";
+				cell.style.textAlign="left";
+				cell.setAttribute("class","middle");
+				cell.innerHTML='<input type="text"  name="registerDate" data-validate=\'{"type":"date","required":true}\' value="'+cars[i].registerDate+'"'
+					+'  style="border:none;background:none;width:6.5em;padding:0 0 0 2px;" class="ignore bc-date ui-widget-content" >';
+				//绑定日期选择
+				bc.form.initCalendarSelect($(cell));
+				
+				//插入合同期限
+				var ccEndDate='';
+				if(!(cars[i].ccEndDate==null))
+					ccEndDate=cars[i].ccEndDate.substring(0,10);
+				cell=newRow.insertCell(5);
+				cell.style.padding="0";
+				cell.style.textAlign="left";
+				cell.setAttribute("class","middle");
+				cell.innerHTML='<input type="text" name="ccEndDate" data-validate=\'{"type":"date","required":true}\' value="'+ccEndDate+'"'
+					+'  style="border:none;background:none;width:6.5em;padding:0 0 0 2px;" class="ignore bc-date ui-widget-content" >';
+				//绑定日期选择
+				bc.form.initCalendarSelect($(cell));
+				
+				//插入商业结束日期
+				var commerialEndDate='';
+				if(!(cars[i].commerialEndDate==null))
+					commerialEndDate=cars[i].commerialEndDate.substring(0,10);
+				cell=newRow.insertCell(6);
+				cell.style.padding="0";
+				cell.style.textAlign="left";
+				cell.setAttribute("class","middle");
+				cell.innerHTML='<input type="text" name="commerialEndDate" data-validate=\'{"type":"date","required":true}\' value="'+commerialEndDate+'"'
+					+'  style="border:none;background:none;width:6.5em;padding:0 0 0 2px;" class="ignore bc-date ui-widget-content"  >';
+				//绑定日期选择
+				bc.form.initCalendarSelect($(cell));
+				
+				//插入强险结束日期
+				var greenslipEndDate='';
+				if(!(cars[i].greenslipEndDate==null))
+					greenslipEndDate=cars[i].greenslipEndDate.substring(0,10);
+				cell=newRow.insertCell(7);
+				cell.style.padding="0";
+				cell.style.textAlign="left";
+				cell.setAttribute("class","middle");
+				cell.innerHTML='<input type="text" name="greenslipEndDate" data-validate=\'{"type":"date","required":true}\' value="'+greenslipEndDate+'"'
+					+'  style="border:none;background:none;width:6.5em;padding:0 0 0 2px;" class="ignore bc-date ui-widget-content" >';
+				//绑定日期选择
+				bc.form.initCalendarSelect($(cell));
+										
+				//插入预计交车日期
+				var predictReturnDate='';
+				if(!(cars[i].predictReturnDate==null))
+					predictReturnDate=cars[i].predictReturnDate.substring(0,10);
+				cell=newRow.insertCell(8);
+				cell.style.padding="0";
+				cell.style.textAlign="left";
+				cell.setAttribute("class","middle");
+				cell.innerHTML='<input type="text" name="predictReturnDate" data-validate=\'{"type":"date","required":true}\' value="'+predictReturnDate+'"'
+					+'  style="border:none;background:none;width:6.5em;padding:0 0 0 2px;" class="ignore bc-date ui-widget-content"  >';
+				//绑定日期选择
+				bc.form.initCalendarSelect($(cell));
+				
+				//插入空白列
+				cell=newRow.insertCell(9);
+				cell.style.minWidth="0.01em";
+				cell.style.borderLeftWidth="0";
+				cell.setAttribute("class","last");
+			}		
+		}
+		
+		function selectUnit(cell){
+			var $cell=$(cell);
+			$cell.click(function(){
+				bc.identity.selectUnit({
+					onOk : function(unit) {
+						$cell.find(":input[name='unitCompanyId']").val(unit.id);
+						$cell.find(":input[name='unitCompany']").val(unit.name);
+					}
+				});
+			})
+		}
 	},
 	buildFormData : function(){
 		$form = $(this);
@@ -283,7 +294,7 @@ bswf.confirmretiredcars.GatherCarsForm = {
 
 		for(var i=0;i<$cars.length;i++){
 			if(verifyUnitId != $cars[i].unitCompanyId){
-				bc.msg.alert("只能确认相同分公司的车辆信息！");
+				bc.msg.alert("必须选择相同分公司的车辆信息！");
 				return false;
 			}
 		}
--- confirmretiredcars/VerifyDate.js
+++ confirmretiredcars/VerifyDate.js
@@ -13,11 +13,13 @@ bswf.confirmretiredcars.VerifyDateForm = {
 				id: $inputs[0].value,
 				plateNo: $inputs[1].value,
 				isClaim: $inputs[2].value,
-				sureReturnDate: $inputs[3].value
+				executionType: $inputs[3].value,
+				sureReturnDate: $inputs[4].value
 			};
 			cars.push(car);
 		});
 		$form.find(":input[name='list_vd_cars']").val($.toJSON(cars));
+		$form.find(":input[name='list_vd_cars_gl']").val($.toJSON(cars));
 	},
 	
 	/** 表单验证方法 */

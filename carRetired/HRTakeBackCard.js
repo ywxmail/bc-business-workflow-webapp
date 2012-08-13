82dda0eabb0d5eef3f748117c32522940ae42689
fatfish <fatfish2010@sina.cn>
2012-08-13 15:52:09 +0800

    交车处理流程人力资源组表单修改

--- carRetired/HRTakeBackCard.js
+++ carRetired/HRTakeBackCard.js
@@ -2,6 +2,22 @@ bc.namespace("bswf.carRetired");
 bswf.carRetired.HRTakeBackCardForm = {
 	init : function(option,readonly){
 		var $form = $(this);
+		var driverName=$form.find(":input[name='htcDriverName']").val();
+		var driverFWZG=$form.find(":input[name='htcDriverFWZG']").val();
+		var driverAndFWZG='';
+				
+		if(typeof(driverName)!="undefined" && typeof(driverFWZG)!="undefined" && driverName != '' && driverFWZG != ''){
+			var dnArr=driverName.split(",");
+			var dnFWZG=driverFWZG.split(",");
+			if(dnArr.length==dnFWZG.length){
+				for(var i=0;i<dnArr.length;i++){
+					driverAndFWZG+=dnArr[i]+','+dnFWZG[i]+','+'已回收;'
+				}
+			}
+		}
+		
+		if(driverAndFWZG!='')
+			$form.find(":input[name='htbcDriverAndFWZG']").val(driverAndFWZG);
 	},
 	buildFormData : function(){
 		$form = $(this);

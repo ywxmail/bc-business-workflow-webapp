0b89b2a49af77a2196789e96b7424e64efddd830
fatfish <fatfish2010@sina.cn>
2012-09-21 11:02:23 +0800

    交车汇总流程增加交车日期确认提示

--- confirmretiredcars/VerifyDate.js
+++ confirmretiredcars/VerifyDate.js
@@ -29,6 +29,45 @@ bswf.confirmretiredcars.VerifyDateForm = {
 		
 		bswf.confirmretiredcars.VerifyDateForm.buildFormData.call(this);
 		
-		return true;
+		//验证确认交车日期是否出现早于当前日期情况
+		//比较日期方法
+		 function duibi(now,predictReturnDate) {
+			    var arr = now.split("-");
+			    var starttime = new Date(arr[0], arr[1], arr[2]);
+			    var nowtimes = starttime.getTime();
+	
+			    var arrs = predictReturnDate.split("-");
+			    var lktime = new Date(arrs[0], arrs[1], arrs[2]);
+			    var predictReturnDatetimes = lktime.getTime();
+	
+			    if (nowtimes >= predictReturnDatetimes) {
+			        return true;
+			    }
+			    else
+			        return false;
+			}
+		
+		var cars = $form.find(":input[name='list_vd_cars']").val();
+		var $cars=eval('('+cars+')');
+		
+		//系统当前日期
+		var now2d=$form.find(":input[name='now2d']").val();
+		//标记出现情况的索引字符串
+		var indexStr="";
+		for(var i=0;i<$cars.length;i++){
+			if(duibi(now2d,$cars[i].sureReturnDate)){
+				var index=i+1;
+				if(indexStr.length==0){
+					indexStr+=index;
+				}else
+					indexStr+='、'+index;
+			}
+		}
+		
+		if(indexStr.length>0){
+			return '第'+indexStr+'条车辆信息的确认交车日期早于当前日期，需要重新编辑信息请点击“否”按钮，不需编辑请点击“是”按钮完成办理此任务。';
+		}else{
+			return true;
+		}
 	}
 };
